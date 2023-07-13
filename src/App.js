2989d4557110827d8bc61bd275c0799aeb1b2665
satheesh <sknaidu8186@gmail.com>
2023-07-13 06:26:55 -0400

    pagination pushed

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import logo from './logo.svg';
 // import './App.css';
 //import ListExample from './list/ListExample';
 //import ProductList from './fetchListApi/ProductList';
-import UseMemoHooks from './memos/UseMemoHooks';
+//import UseMemoHooks from './memos/UseMemoHooks';
+import Paggination from './pagination';
 function App() {
   return (
     <div >
-      <UseMemoHooks />
+      <Paggination />
     </div>
   );
 }
